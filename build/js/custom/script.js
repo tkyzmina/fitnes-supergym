de7c230881389915d217bf13a524db1b705b47ff
tkyzmina <tkyzmina@gmail.com>
2020-07-15 13:11:27 +0300

    корректировка согласно замечаниям наставника v1

--- build/js/custom/script.js
+++ build/js/custom/script.js
@@ -51,6 +51,13 @@
       ]
     });
   });
+  // // imask
+  var element = document.getElementById('tel');
+  var maskOptions = {
+    mask: '+{7}(000)000-00-00'
+  };
+  var mask = IMask(element, maskOptions);
+  // imask end
 
 
   var coaches = document.querySelectorAll('.coaches__item');
